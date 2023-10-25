36f5a63e70cb64c78fb33d2b6b61804ca8600a0b
akkasocrates <me.llamo.jjulian@gmail.com>
2023-10-25 08:44:12 +0200

    25/10/2023

--- josejulianSaavedra_procesos3.py
+++ josejulianSaavedra_procesos3.py
@@ -2,31 +2,34 @@
 import multiprocessing
 
 def escribir_en_fichero(texto, nombre_fichero):
-    fichero = open(nombre_fichero, "a")
-    fichero.write(texto, "\n")
-    fichero.close
+     with open(nombre_fichero, "a") as fichero:
+        fichero.write(texto + "\n")
 
 # Función para el proceso lector
 def leer_fichero_y_mostrar(nombre_fichero):
-    fichero = open(nombre_fichero, 'a') 
-    contenido = fichero.read()
-    print("Contenido del fichero:")
-    print(contenido)
+    with open(nombre_fichero, 'r') as fichero:  
+        contenido = fichero.read()
+        print("Contenido del fichero:")
+        print(contenido)
 
-while True:
-    texto = input("Escribe un texto: (cuando escribas -1 termina el programa)")
+if __name__ == "__main__":
+
     nombre_fichero = "fichero.txt"
     
-    if texto == "-1":
-        break
+    while True:
+        texto = input("Escribe un texto: (cuando escribas -1 termina el programa)")
     
-    proceso_escritor = multiprocessing.Process(target=escribir_en_fichero, args=(texto, nombre_fichero))
-    proceso_escritor.start()
-    proceso_escritor.join()
+        if texto == "-1":
+            break
+        else:
     
-    proceso_lector = multiprocessing.Process(target=leer_fichero_y_mostrar, args=nombre_fichero)
-    proceso_lector.start()
-    proceso_lector.join()
+            proceso_escritor = multiprocessing.Process(target=escribir_en_fichero, args=(texto, nombre_fichero))
+            proceso_escritor.start()
+            proceso_escritor.join()
+    
+            proceso_lector = multiprocessing.Process(target=leer_fichero_y_mostrar, args=(nombre_fichero,))
+            proceso_lector.start()
+            proceso_lector.join()
     
 
 print("Fin del programa")
